ef79d36778ed7e1ef468819f4fdb99fddfa356f4
Peter Doherty <pdoherty926@gmail.com>
2017-05-17 13:08:35 -0400

    chapter 9 - Filtering Lists exercises

--- chapter9.hs
+++ chapter9.hs
@@ -58,3 +58,93 @@ myWords' "foo bar baz"
 
 myLines' lines = myThing '\n' lines
 myLines' sentences
+
+mySqr = [x^2 | x <- [1..5]]
+
+-- 4, 16
+one = [x | x <- mySqr, rem x 2 == 0]
+
+-- [] -- no squares > 50
+two = [(x, y) | x <- mySqr, y <- mySqr, x < 50, y > 50]
+
+-- [] -- still no squares > 50
+three = take 5 [(x, y) | x <- mySqr, y <- mySqr, x < 50, y > 50 ]
+
+-- return vowels
+myString xs = [x | x <- xs, elem x "aeiou"]
+myString "a sad lad is a bad dad"
+
+mySqr = [x^2 | x <- [1..5]]
+myCube = [y^3 | y <- [1..5]]
+
+-- 1
+myTuples = [(x, y) | x <- mySqr, y <- myCube]
+
+-- 2
+mySmallTuples = [(x, y) | x <- mySqr, y <- myCube, x < 50, y < 50]
+
+-- 3
+mySmallTuplesLength = length mySmallTuples -- 15
+
+
+blah = enumFromTo 'a' 'z'
+-- :sprint blah -- _ => totally unevaluated
+
+take 1 blah
+-- :sprint blah -- 'a':_ => partially unevaluated
+
+take 2 blah
+-- :sprint blah -- 'a' : 'b' :_ => partially unevaluated
+
+-- Normal Form => expression fully evaluated 2 * 2 = 4
+-- Weak head normal form (WHNF) is a larger set and contains both the possibility
+-- that the expression is fully evaluated (normal form) and the possibility that
+-- the expression has been evaluated to the point of arriving at a data
+-- constructor or lambda awaiting an argument.
+
+tup = (1, 2) -- WHNF & NF => anything in normal form is also in WHNF
+tup2 = (1, 2 + 2) -- WHNF => + hasn't been evaluated yet
+lam = \x -> x * 10 -- WHNF & NF => can't be reduced further until lamda is applied
+
+-- Bottom Madness
+-- 1 / blow up
+-- 2 / value
+-- 3 / blow up
+-- 4 / value
+-- 5 / blow up
+-- 6 / value
+-- 7 / blow up
+-- 8 / value
+-- 9 / value
+-- 10 / blow up
+
+-- Is it normal form?
+-- 1 / NF
+-- 2 / WHNF
+-- 3 / WHNF
+-- 4 / WHNF
+-- 5 / neither
+-- 6 / WHNF
+-- 7 / NF
+
+-- A common mantra for performance sensitive code in Haskell is, “lazy in the
+-- spine, strict in the leaves.”
+
+-- More Bottoms
+-- 1 / blow up
+-- 2 / value
+-- 3 / blow up
+
+-- 4
+-- maps chars in xs to bools based on vowel/consonant
+itIsMystery :: String -> [Bool]
+itIsMystery xs = map (\x -> elem x "aeiou") xs
+
+-- 5
+-- a / raise each of 1..10 to the power of 2
+-- b / [1, 10, 20]
+-- c / [15, 15, 15]
+
+-- 6
+import Data.Bool (bool)
+result2 = map (\x -> bool x (-x) (x == 3)) [1..10] -- [1,2,-3,4,5,6,7,8,9,10]
