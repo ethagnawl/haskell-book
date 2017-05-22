80fc43da98a2105923284b947501a60c5be64fef
Peter Doherty <pdoherty926@gmail.com>
2017-05-22 00:41:09 -0400

    wrapping up chapter 10

--- chapter10.hs
+++ chapter10.hs
@@ -169,3 +169,72 @@ preciseAvgWordLength :: String -> Double
 preciseAvgWordLength str = dividend / divisor
   where dividend = fromIntegral (sum (map length (words str))) :: Double
         divisor = fromIntegral (length (words str)) :: Double
+
+-- Rewriting functions using folds
+
+-- 1. myOr returns True if any Bool in the list is True.
+myOr :: [Bool] -> Bool
+myOr = foldr (||) False
+
+-- 2. myAny returns True if a -> Bool applied to any of the values in the list
+-- returns True.
+myAny :: (a -> Bool) -> [a] -> Bool
+myAny func = foldr (\x xs -> xs || func x) False
+
+-- 3. Write two versions of myElem. One version should use folding and the
+-- other should use any.
+myElemFold :: Eq a => a -> [a] -> Bool
+myElemFold needle = foldr (\x xs -> xs || x == needle) False
+
+myElemAny:: Eq a => a -> [a] -> Bool
+myElemAny needle = any (== needle)
+
+-- 4. Implement myReverse, don’t worry about trying to make it lazy.
+myReverse :: [a] -> [a]
+myReverse = foldl (flip (:)) []
+-- myReverse "blah" -- "halb"
+-- myReverse [1..5] -- [5,4,3,2,1]
+
+-- 5. Write myMap in terms of foldr. It should have the same behavior as the
+-- built-in map.
+myMap :: (a -> b) -> [a] -> [b]
+myMap func = foldr (\x xs -> func x : xs) []
+
+-- 6. Write myFilter in terms of foldr. It should have the same behavior as
+-- the built-in filter.
+myFilter :: (a -> Bool)
+myFilter func = foldr (\x xs -> if func x then x : xs else xs) []
+
+-- 7. squish flattens a list of lists into a list
+squish :: [[a]] -> [a]
+squish = foldr (++) []
+
+-- 8. squishMap maps a function over a list and concatenates the results.
+squishMap :: (a -> [b]) -> [a] -> [b]
+squishMap func lst = foldr (++) [] $ map func lst
+
+-- 9. squishAgain flattens a list of lists into a list. This time re-use the
+-- squishMap function.
+squishAgain :: [[a]] -> [a]
+squishAgain = concat . map id
+
+-- 10. myMaximumBy takes a comparison function and a list and returns
+-- the greatest element of the list based on the last value that the
+-- comparison returned GT for.
+-- https://github.com/dwayne/haskell-programming/pull/1
+myMaximumBy :: (a -> a -> Ordering) -> [a] -> a
+myMaximumBy func xs = foldr compare' seed xs
+  where seed = (last xs)
+        compare' l r =  case (func l r) of
+                          GT -> l
+                          _ -> r
+
+-- 11. myMinimumBy takes a comparison function and a list and returns the
+-- least element of the list based on the last value that the comparison
+-- returned LT for.
+myMinimumBy :: (a -> a -> Ordering) -> [a] -> a
+myMinimumBy func xs = foldr compare' seed xs
+  where seed = (last xs)
+        compare' l r =  case (func l r) of
+                          GT -> r
+                          _ -> l
