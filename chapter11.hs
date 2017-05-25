f61b59a9da1c0e05910f5469d1befbc7f9dbf18c
Peter Doherty <pdoherty926@gmail.com>
2017-05-24 20:58:07 -0400

    chapter 11 - Vigenere Cipher

--- chapter11.hs
+++ chapter11.hs
@@ -426,3 +426,43 @@ f Friday = "Miller Time"
 g xs = xs !! (length xs - 1)
 -- c / g delivers the final element of xs
 
+-- Vigenère cipher
+
+-- It looks like there's an off-by-one bug in my caeser cipher, so I'm
+-- borrowing @dwayne's
+-- github.com/dwayne/haskell-programming/blob/9af6fc2a106640b8f4eb7504f29fcffb89dd071d/ch9/Cipher.hs
+-- The following only works on lowercase strings without spaces. Sorry.
+
+import Data.Char
+
+encode :: Char -> Int
+encode ch = ord ch - ord 'a'
+
+decode :: Int -> Char
+decode n = chr (ord 'a' + n)
+
+shift :: (Int -> Int -> Int) -> Int -> Char -> Char
+shift f n ch = decode $ mod (f (encode ch) n) 26
+
+rightShift :: Int -> Char -> Char
+rightShift = shift (+)
+
+leftShift :: Int -> Char -> Char
+leftShift = shift (-)
+
+e :: Int -> String -> String
+e n = map (rightShift n)
+
+d :: Int -> String -> String
+d n = map (leftShift n)
+
+crypt string keyword direction = concatMap func sks
+  where base = ord 'a'
+        offset = (\x -> (ord x) - base)
+        func = (\(s, k) -> direction (offset k) [s])
+        keywords = take (length string) $ cycle keyword
+        sks = zip string keywords
+
+encrypt string keyword = crypt string keyword e
+
+decrypt string keyword = crypt string keyword d
