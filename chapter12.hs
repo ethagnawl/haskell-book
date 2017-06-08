3f2c907f3645cf80db29e889f3de9d21577ebe6e
Peter Doherty <pdoherty926@gmail.com>
2017-06-08 14:12:01 -0400

    chapter 12 - Small library for Maybe

--- chapter12.hs
+++ chapter12.hs
@@ -106,3 +106,74 @@ integerToNat int =  if int < 0
   where reduceNatToZero 0 = Zero
         reduceNatToZero n = Succ (reduceNatToZero (n - 1))
 
+-- Small library for Maybe
+
+-- 1. Simple boolean checks for Maybe values.
+
+-- isJust (Just 1) -- True
+-- isJust Nothing -- False
+isJust :: Maybe a -> Bool
+isJust (Just _) = True
+isJust Nothing = False
+
+-- isNothing (Just 1) -- False
+-- isNothing Nothing -- True
+isNothing :: Maybe a -> Bool
+isNothing m = not $ isJust m
+
+-- 2. The following is the Maybe catamorphism. You can turn a Maybe
+
+-- value into anything else with this.
+-- mayybee 0 (+1) Nothing -- 0
+-- mayybee 0 (+1) (Just 1) -- 2
+
+mayybee :: b -> (a -> b) -> Maybe a -> b
+mayybee seed func m = if (isNothing m) then seed else compute m
+  where compute (Just x) = func x
+
+mayybee' :: b -> (a -> b) -> Maybe a -> b
+mayybee' b _ Nothing = b
+mayybee' _ f (Just v) = f v
+
+-- 3. In case you just want to provide a fallback value.
+-- fromMaybe 0 Nothing -- 0
+-- fromMaybe 0 (Just 1) -- 1
+fromMaybe :: a -> Maybe a -> a
+fromMaybe seed Nothing = seed
+fromMaybe seed (Just v) = v
+
+-- Try writing it in terms of the maybe catamorphism
+fromMaybe' seed m = mayybee seed id m
+
+-- 4. Converting between List and Maybe.
+
+-- listToMaybe [1, 2, 3] -- Just 1
+-- listToMaybe [] -- Nothing
+
+listToMaybe :: [a] -> Maybe a
+listToMaybe [] = Nothing
+-- Doesn't really specify _which_ a its interested in.
+listToMaybe (head':_) = Just head'
+
+-- maybeToList (Just 1) -- [1]
+-- maybeToList Nothing -- []
+maybeToList :: Maybe a -> [a]
+maybeToList Nothing = []
+maybeToList (Just x) = [x]
+
+-- 5. For when we just want to drop the Nothing values from our list.
+-- catMaybes [Just 1, Nothing, Just 2] -- [1, 2]
+-- catMaybes [Nothing, Nothing, Nothing] -- []
+catMaybes :: [Maybe a] -> [a]
+catMaybes lst = [a | (Just a) <- lst]
+
+-- 6. You’ll see this called “sequence” later.
+-- flipMaybe [Just 1, Just 2, Just 3] -- Just [1, 2, 3]
+-- flipMaybe [Just 1, Nothing, Just 3] -- Nothing
+flipMaybe :: [Maybe a] -> Maybe [a]
+flipMaybe lst = if anyNothings
+                  then Nothing
+                  else Just values
+  where
+    anyNothings = any isNothing lst
+    values = catMaybes lst
