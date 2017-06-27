115dc84b4d0155ac6e0add3efa0b90c04068b01c
Peter Doherty <pdoherty926@gmail.com>
2017-06-27 18:59:02 -0400

    chapter 15 - madlibs

--- chapter15.hs
+++ chapter15.hs
@@ -16,3 +16,50 @@ instance Monoid a => Monoid (Optional a) where
 (Only (Sum 1) `mappend` Nada) == (Only (Sum {getSum = 1})) -- True
 (Only [1] `mappend` Nada) == (Only [1]) -- True
 (Nada `mappend` Only (Sum 1)) == (Only (Sum {getSum = 1})) -- True
+
+-- Associativity simply says that you can associate the arguments
+-- of your operation differently and the result will be the same.
+
+-- Commutative means you can reorder the arguments and still get the same
+-- result.
+
+-- Addition and multiplication are commutative, but (++) for the list type
+-- is only associative.
+
+-- Monoid abides by the law of associativity but not the law of commutativity,
+-- even though some monoidal operations (addition and multiplication) are commutative.
+
+-- Within an expression containing two or more occurrences in a row of the same
+-- associative operator, the order in which the operations are performed does not
+-- matter as long as the sequence of the operands is not changed. That is,
+-- rearranging the parentheses in such an expression will not change its value.
+-- Consider the following equations:
+
+-- ( 2 + 3 ) + 4 = 2 + ( 3 + 4 ) = 9
+-- 2 × ( 3 × 4 ) = ( 2 × 3 ) × 4 = 24
+
+-- the binary operation must be associative and it must have a sensible identity value.
+
+-- Madlibs
+
+import Data.Monoid
+
+type Verb = String
+type Adjective = String
+type Adverb = String
+type Noun = String
+type Exclamation = String
+
+madlibbinBetter' :: Exclamation -> Adverb -> Noun -> Adjective -> String
+madlibbinBetter' e adv noun adj =
+  mconcat
+    [
+      e,
+      "! he said ",
+      adv,
+      " as he jumped into his car ",
+      noun,
+      " and drove off with his ",
+      adj,
+      " wife."
+    ]
